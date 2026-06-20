eb7ac5fc49f050df675c9b48660d82e127ba6fad
ToPIC-pro <glebtukmachev@gmail.com>
2026-06-20 12:06:08 +0300

    Add files via upload

--- run.py
+++ run.py
@@ -1,11 +1,7 @@
 import os
-import sys
 
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
-print(f"[DEBUG] Рабочая директория: {os.getcwd()}")
-print(f"[DEBUG] tracks есть? {os.path.exists('tracks')}")
-print(f"[DEBUG] config.json есть? {os.path.exists('config.json')}")
 
 from menu import Menu
 
